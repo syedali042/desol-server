6feae0a5a30e0927345c90230ae2285e048acbe1
Syed <salizahid75@gmail.com>
2022-04-05 01:43:53 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use((req, res, next) => {
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 // body-parser
