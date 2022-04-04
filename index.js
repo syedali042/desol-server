6e38346f394bcc2d93f408098dd0884bbed45369
Syed <salizahid75@gmail.com>
2022-04-05 01:34:27 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
 // Cross Platform Compatibility
-// app.use(cors());
-const corsOptions = {
-    origin: 'https://desol-client.vercel.app/',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+app.use(cors());
+// const corsOptions = {
+//     origin: 'https://desol-client.vercel.app/',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 // body-parser
 app.use(express.json());
 // setting routes
